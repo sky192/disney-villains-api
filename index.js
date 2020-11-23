90f01b06e5c427861b831ba1460b1eca9749afe1
sky192 <samahehloghmani@gmail.com>
2020-11-22 20:45:09 -0500

    modified index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { getAllVillains, getVillainBySlug, saveNewVillain } = require('./controllers/villains')
 
 const app = express()
+
+app.get('/villains', getAllVillains)
+
+app.get('/villains/:slug', getVillainBySlug)
+
+app.post('/villains', bodyParser.json(), saveNewVillain)
+
+app.listen(1337, () => {
+  // eslint-disable-next-line no-console
+  console.log('Listening on port 1337...')
+})
